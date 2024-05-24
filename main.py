c95a57df85b96f59f532667c2830d2b90ea7149d
akramfdk <akramjeet.singh@gmail.com>
2024-05-24 21:01:29 +1000

    first commit - using method 1

---
+++ main.py
@@ -0,0 +1,71 @@
+import turtle
+import random
+
+turtle.setworldcoordinates(0, 0, 100, 100)
+
+NUM_TURTLES = 6
+
+# create a colors list (equal in number to turtles) and assign corresponding color to turtles
+colors = ["violet", "indigo", "blue", "green", "orange", "red"]
+
+# create different turtles, create a turtles list and append turtles to it
+turtles = []
+for index in range(NUM_TURTLES):
+    pen = turtle.Turtle()
+    pen.speed("fastest")
+    pen.shape("turtle")
+    pen.up()
+    pen.color(colors[index])
+    pen.setpos(0, 20 + index * 10)
+    pen.speed("slow")
+    turtles.append(pen)
+
+
+def turtle_reaches_end():
+    x_coordinates = []
+    for t in turtles:
+        x_coordinates.append(t.xcor())
+
+    max_x_val = max(x_coordinates)
+    if max_x_val >= 100:
+        winner_index = x_coordinates.index(max_x_val)
+        return winner_index
+
+    return None
+
+
+def race():
+    continue_race = True
+
+    victor = ""
+    while continue_race:
+        for ind in range(len(turtles)):
+            turtles[ind].setx(turtles[ind].xcor() + random.randint(1, 3))
+
+        victor = turtle_reaches_end()
+        if victor:
+            continue_race = False
+
+    return victor
+
+
+# pop up a dialog box to guess the color that will win
+guess = ""
+while not (guess in colors):
+    guess = turtle.textinput("Predict Winner", "Which color turtle will win?")
+
+# after the input, the race takes place and one color will win
+winner = race()
+
+# display appropriate message depending on whether the chosen color won or lost
+if colors[winner] == guess:
+    print(f"You win. {colors[winner]} turtle wins!!")
+else:
+    print(f"You lose. {colors[winner]} turtle wins!!")
+
+
+# display the screen
+my_screen = turtle.Screen()
+# place the turtles on the screen to the left
+
+my_screen.exitonclick()
